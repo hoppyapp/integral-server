d28e2502853ac6a62655c1b14c6d725c2263b34d
David Gaspar <davidgaspar.dev@gmail.com>
2020-07-06 16:50:59 -0300

    Added Measures method and removed code commet

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,7 @@
 import Mongo from "../databases/mongo";
 import userSchema from "../databases/mongo/schemas/user";
-import { ResponseData, User } from "../helpers/utils/types";
-import { Timestamp } from "mongodb";
+import { ResponseData, User, Measure } from "../helpers/utils/types";
+import { ObjectId } from "mongodb";
 
 /**
  * Server Service
@@ -25,34 +25,6 @@ export default class UserService extends Mongo {
     }
 
     /**
-     * Login
-     * 
-     * @param {} data
-     */
-    // public async login(data: any): Promise<ResponseData> {
-    //     try {
-
-    //         return {
-    //             status: 200, 
-    //             content: {
-    //                 name: "logged",
-    //                 data: data
-    //             }
-    //         }
-
-    //     } catch(e) {
-
-    //         return {
-    //             status: 500,
-    //             content: {
-    //                 name: e.name,
-    //                 description: e.message
-    //             }
-    //         }
-    //     }
-    // }
-
-    /**
      * Register 
      * 
      * @param {} data
@@ -60,21 +32,13 @@ export default class UserService extends Mongo {
     public async register(data: User): Promise<ResponseData> {
         try {
 
-            console.log(data);
+            console.log(UserService.LOG_TAG, data);
 
             const dataExists: User | null = await super.readOneData<User>({ email: data.email });
 
-            console.log("data exists: " + dataExists);
+            console.log(UserService.LOG_TAG, "data exists: " + dataExists);
 
             if(!dataExists) {
-                // data.measures = data.measures.map<any>((m: any) => {
-
-                //     if(m.date) m.date = new Timestamp(m.date, 1);
-                //     return m;
-                // });
-
-                // console.log(data.measures[0].date);
-
                 await super.createData<User>(data);
             }
 
@@ -89,6 +53,58 @@ export default class UserService extends Mongo {
         } catch(e) {
 
             console.log(e);
+
+            return {
+                status: 500,
+                content: {
+                    name: e.name,
+                    description: e.message
+                }
+            }
+        }
+    }
+
+    /**
+     * Add measures
+     * 
+     * @param {string} userId 
+     * @param {Measure[]} measures 
+     */
+    public async addMeasures(userId: string, data: Buffer): Promise<ResponseData> {
+        try {
+
+            /// Measure
+            let measures: Measure[] = JSON.parse(data.toString());
+            let _id: ObjectId = new ObjectId(userId);
+
+            const user: User | null = await super.readOneData<User>({ _id });
+
+            if(!user) throw Error("User not found");
+
+            measures = measures.filter((m: Measure) => {
+                for(let measure of user.measures) {
+                    if(m.date.day === measure.date.day && m.date.month === measure.date.month && m.date.year === measure.date.year) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+
+            user.measures = user.measures.concat(measures);
+
+            await super.updateOneData<User>(_id, { $set: { measures: user.measures }});
+
+            return {
+                status: 200,
+                content: {
+                    name: "Added measure",
+                    data: user
+                }
+            }
+
+        } catch(e) {
+
+            console.error(e);
 
             return {
                 status: 500,
